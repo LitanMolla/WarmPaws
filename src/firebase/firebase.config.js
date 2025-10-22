a647f358cb44c1da62679ee84c4c7262040dcf7a
LitanMolla <litanmolla9@gmail.com>
2025-10-23 02:12:56 +0600

    .env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvAUpHAuq8kVNPTtSGM5AXZVjobquwc4c",
-  authDomain: "warmpaws-a5454.firebaseapp.com",
-  projectId: "warmpaws-a5454",
-  storageBucket: "warmpaws-a5454.firebasestorage.app",
-  messagingSenderId: "72607266667",
-  appId: "1:72607266667:web:3e96bf7141a99e39b63e2e"
+  apiKey:  import.meta.env.VITE_apiKey,
+  authDomain:  import.meta.env.VITE_authDomain,
+  projectId:  import.meta.env.VITE_projectId,
+  storageBucket:  import.meta.env.VITE_storageBucket,
+  messagingSenderId:  import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId 
 };
 
 const app = initializeApp(firebaseConfig);
