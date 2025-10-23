176d045371f99b2a4da58101f52f1baa9b11f5da
LitanMolla <litanmolla9@gmail.com>
2025-10-23 16:09:49 +0600

    added Winter Care section problem fix

--- src/components/PetCaresWinter/PetCaresWinter.jsx
+++ src/components/PetCaresWinter/PetCaresWinter.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React, { use } from 'react'
+import Container from '../Container/Container'
+import PetsCareCard from '../petsCareCard/petsCareCard'
+const careDataPromise = fetch('/petsCare.json').then(res => res.json())
 const PetCaresWinter = () => {
-  return (
-    <div>PetCaresWinter</div>
-  )
+    const data = use(careDataPromise)
+    return (
+        <>
+            <div className="bg-orange-50 py-10 md:py-20">
+                <Container>
+                    <h4 className='text-3xl font-medium text-center mb-4'>Winter Care Tips for Pets</h4>
+                    <p className='text-center text-gray-600 mb-5 max-w-xl mx-auto'>Keep your pets warm, healthy, and happy this winter with simple care tips for nutrition, grooming, and protection from the cold.</p>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                        {data.map(post => <PetsCareCard key={post.id} post={post} />)}
+                    </div>
+                </Container>
+            </div>
+        </>
+    )
 }
 
 export default PetCaresWinter
